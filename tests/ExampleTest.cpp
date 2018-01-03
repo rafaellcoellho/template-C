ef8ce15c76be158e96758a7ec249127f098c2ec2
Rafael Coelho <rafaellcoellho@gmail.com>
2018-01-03 11:07:41 -0300

    feat(tests): add tests to the project

---
+++ tests/ExampleTest.cpp
@@ -0,0 +1,22 @@
+#include "../cpputest/include/CppUTest/TestHarness.h"
+
+extern "C"
+{
+#include "../inc/helloWorld.h"
+}
+
+TEST_GROUP(helloWorld)
+{
+        void setup()
+        {
+        }
+
+        void teardown()
+        {
+        }
+};
+
+TEST(helloWorld, test1)
+{
+    STRCMP_EQUAL("Koe", helloWorld());
+}
